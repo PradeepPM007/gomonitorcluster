4aabff5ce94715f45f5c1b1f7ab260c9e39879eb
PradeepPM007 <pm.pradeep.nh@gmail.com>
2022-10-08 12:52:50 +0530

    added default location to find config

--- gomonitorcluster/main.go
+++ gomonitorcluster/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+	"path/filepath"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -10,16 +12,19 @@ import (
 
 func main() {
 
-	kubeconfig := flag.String("kubeconfig", "", "My kubeconfig path")
+	homepath, _ := os.UserHomeDir()
+	kubeconfig := flag.String("kubeconfig", filepath.Join(homepath, ".kube", "config"), "My kubeconfig path")
 	mynamespace := flag.String("mynamespace", "", "My Namespace Name")
 	mypod := flag.String("mypod", "", "My Pod Name")
 	flag.Parse()
+
+	log.Println("---------------> config path:", *kubeconfig)
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("creating ClientSet")
+	log.Println("---------------> creating ClientSet")
 	ClientSet := kubernetes.NewForConfigOrDie(config)
 
 	if ClientSet == nil {
